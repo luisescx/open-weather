3e973f307b58c10d9a95cb2fad83b433dad9835c
luisescx <luis.esculapio@Live.com>
2021-12-18 17:48:50 -0300

    add theme styles

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,26 @@
+export default {
+    colors: {
+        primary: "#00AAf2",
+
+        secondary: "#FF872C",
+        secondary_light: `${"rgba(255, 135, 44, 0.3)"}`,
+
+        success: "#12A454",
+        success_light: `${"rgba(18, 164, 84, 0.3)"}`,
+
+        danger: "#E83F5B",
+        danger_light: `${"rgba(232, 63, 91, 0.3)"}`,
+
+        shape: "#FFFFFF",
+        title: "#363F5F",
+        text: "#969CB2",
+        text_dark: "#000000",
+        background: "#F0F2F5",
+    },
+
+    fonts: {
+        regular: "Poppins_400Regular",
+        medium: "Poppins_500Medium",
+        bold: "Poppins_700Bold",
+    },
+};
