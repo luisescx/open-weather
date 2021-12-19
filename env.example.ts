82c5a394e59f8d24c7bd56495207b67e8da83b3e
luisescx <luis.esculapio@Live.com>
2021-12-19 15:54:29 -0300

    fix env, apiServices

---
+++ env.example.ts
@@ -0,0 +1,4 @@
+export const env = {
+    openWeatherKey: "",
+    googleApiKey: "",
+};
--- env.ts
+++
@@ -1,4 +0,0 @@
-export const env = {
-    openWeatherKey: "b0c31d992607818c91e84726143de83c",
-    googleApiKey: "AIzaSyBO9DeGYRGvAIEyNfJUVm9IdarMxuOJCzs",
-};
