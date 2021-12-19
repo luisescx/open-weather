ee95823fe881dc7edf3c1e71aacf78483bc9ef10
luisescx <luis.esculapio@Live.com>
2021-12-19 19:53:38 -0300

    add new interfaces

--- src/common/interfaces.ts
+++ src/common/interfaces.ts
@@ -13,12 +13,53 @@ export interface GooglePlaceCity {
     status: string;
 }
 
+export interface OpenWeatherCity {
+    id: string;
+
+    name: string;
+
+    coord: Coordinates;
+
+    weather: {
+        main: string;
+        description: string;
+    }[];
+
+    main: {
+        temp: number;
+        temp_min: number;
+        temp_max: number;
+    };
+}
+
 export interface CityDTO {
     googlePlaceId: string;
+
+    openWeatherId: string;
 
     name: string;
 
     country: string;
+
+    coord: Coordinates;
+
+    weather: {
+        main: string;
+        description: string;
+        temp: number;
+        temp_min: number;
+        temp_max: number;
+    };
+
+    // isFavorite: boolean;
+    // daily: {
+
+    // }[]
+}
+
+interface Coordinates {
+    lon: number;
+    lat: number;
 }
 
 export interface RouteParams {
@@ -28,6 +69,8 @@ export interface RouteParams {
 export interface CardProps {
     isWeather?: boolean;
     city: CityDTO;
+    addCity?: (newCity: CityDTO) => void;
+    loadCities?: () => void;
 }
 
 export type RootParamList = {
